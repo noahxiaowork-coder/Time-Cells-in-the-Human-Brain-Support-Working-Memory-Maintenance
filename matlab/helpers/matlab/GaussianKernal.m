2c5c00a198ac1673f1423b484f569ea1e36b508d
noahxiaowork-coder <noah.xiao.work@gmail.com>
2026-01-27 00:14:55 -0500

    Delete matlab directory

--- matlab/helpers/matlab/GaussianKernal.m
+++
@@ -1,7 +0,0 @@
-function gKer = GaussianKernal(n_sigma, gaussian_sigma)
-    kernel_size = round(n_sigma * gaussian_sigma); %R you changing it?
-    x = -kernel_size:kernel_size;
-    gaussian_kernel = exp(-(x.^2) / (2 * gaussian_sigma^2));
-    gKer = gaussian_kernel / sum(gaussian_kernel);
-end
-
